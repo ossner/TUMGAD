cd2cdc77d98a93b1104bdbdd390400e3c10e3a76
Sebastian Ossner <github@sebastianoner.com>
2020-03-05 14:36:41 +0100

    Create a Terminal class providing necessarcy methods

---
+++ src/DataStructures/Terminal.java
@@ -0,0 +1,41 @@
+package DataStructures;
+
+import java.io.*;
+
+public class Terminal {
+    public static StringBuilder readFile(String fileName) {
+        BufferedReader bufferedReader;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            System.err.println("could not read File (" + fileName + ") (" + e + ")");
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        try {
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                sb.append(line).append("\n");
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.err.println("could not read File (" + fileName + ") (" + e + ")");
+            return null;
+        }
+        return sb;
+    }
+
+    public static void saveToFile(String fileName, StringBuilder toSave) {
+        try {
+            PrintWriter out = new PrintWriter(fileName);
+            out.println(toSave);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
