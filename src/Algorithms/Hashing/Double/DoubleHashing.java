095a7b5baf6b13d81ed9633a66ede1bcc5928d47
Sebastian Ossner <github@sebastianoner.com>
2020-03-09 14:13:13 +0100

    insert first hash function into template

--- src/Algorithms/Hashing/Double/DoubleHashing.java
+++ src/Algorithms/Hashing/Double/DoubleHashing.java
@@ -1,12 +1,16 @@
 package Algorithms.Hashing.Double;
 
+import DataStructures.Terminal;
+
 import java.util.Random;
 
 public class DoubleHashing {
+    static StringBuilder doubleHashingExerciseStringBuilder;
+    static StringBuilder doubleHashingSolutionStringBuilder;
 
 
     public static void main(String[] args) {
-        System.out.println(DoubleHashing.generateHashfunction());
+        generateExercise();
     }
 
     static int[] generateHashfunction() {
@@ -14,8 +18,19 @@ public class DoubleHashing {
         int[] a = new int[2];
         a[0] = (rand.nextInt(10) + 1);
         a[1] = (rand.nextInt(10));
-        String h1 = "h(x) = (" + a[0] + "x + " + a[1]+") mod 11";
         return a;
     }
 
+    static void generateExercise() {
+        doubleHashingExerciseStringBuilder = Terminal.readFile("src/Algorithms/Hashing/Double/DoubleHashingExerciseTemplate.tex");
+        doubleHashingSolutionStringBuilder = Terminal.readFile("src/Algorithms/Hashing/Double/DoubleHashingSolutionTemplate.tex");
+
+        int[] a = generateHashfunction();
+
+        Terminal.replaceinSB(doubleHashingExerciseStringBuilder, "NORMALFUNCTION", "h(x) = (" + a[0] + "x + " + a[1] + ") \\mod 11");
+        Terminal.replaceinSB(doubleHashingSolutionStringBuilder, "NORMALFUNCTION", "h(x) = (" + a[0] + "x + " + a[1] + ") \\mod 11");
+
+        Terminal.saveToFile("src/Algorithms/Hashing/Double/DoubleHashingExercise.tex", doubleHashingExerciseStringBuilder);
+        Terminal.saveToFile("src/Algorithms/Hashing/Double/DoubleHashingSolution.tex", doubleHashingSolutionStringBuilder);
+    }
 }
