fc7497c35fc4a2146179913d8c84329d74764a92
Sebastian Ossner <github@sebastianoner.com>
2020-03-09 02:00:46 +0100

    finish generation for mergesort for now

--- src/Algorithms/Sorting/MergeSort/MergeSort.java
+++ src/Algorithms/Sorting/MergeSort/MergeSort.java
@@ -2,73 +2,44 @@ package Algorithms.Sorting.MergeSort;
 
 import DataStructures.Terminal;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MergeSort {
-    static String[] mergeString = new String[4];
+    static StringBuilder mergeSortExerciseStringBuilder;
+    static StringBuilder mergeSortSolutionStringBuilder;
 
-    protected static void sort(int[] numbers, int l, int r, int depth) {
-        if (l >= r) {
-            return;
-        }
+    public static void generateExercise() {
+        int[] a = generateRandomMSArray();
+        mergeSortExerciseStringBuilder = Terminal.readFile("src/Algorithms/Sorting/MergeSort/MergeSortExerciseTemplate.tex");
+        mergeSortSolutionStringBuilder = Terminal.readFile("src/Algorithms/Sorting/MergeSort/MergeSortSolutionTemplate.tex");
 
-        int m = (l + r) / 2;
-        if (mergeString[depth] == null) {
-            mergeString[depth] = mergePrint(numbers, l, m) + " | ";
-        } else {
-            mergeString[depth] += " | " + mergePrint(numbers, m + 1, r);
-        }
-        sort(numbers, l, m, depth + 1);
-        sort(numbers, m + 1, r, depth + 1);
-        merge(numbers, l, m, r);
-        System.out.println(Terminal.printArray(numbers));
-    }
+        StringBuilder exerciseStringBuilder = Terminal.readFile("docs/Exercises.tex");
+        StringBuilder solutionStringBuilder = Terminal.readFile("docs/Solutions.tex");
 
-    private static String mergePrint(int[] numbers, int l, int r) {
-        String ret = "" + numbers[l++];
-        for (; l <= r; l++) {
-            ret += ", " + numbers[l];
-        }
-        return ret;
-    }
 
-    protected static void merge(int[] numbers, int l, int m, int r) {
-        // Beide Hälften verschmelzen
-        int j = l, k = m + 1;
-        int[] buffer = new int[r - l + 1];
-        for (int i = 0; i <= r - l; i++) {
-            if (j > m) { // linker Teil leer
-                buffer[i] = numbers[k];
-                k++;
-            } else if (k > r) { // rechter Teil leer
-                buffer[i] = numbers[j];
-                j++;
-            } else if (numbers[j] <= numbers[k]) {
-                buffer[i] = numbers[j];
-                j++;
-            } else {
-                buffer[i] = numbers[k];
-                k++;
-            }
-        }
+        Terminal.replaceinSB(mergeSortExerciseStringBuilder, "$INITARRAY$", Terminal.printArray(a));
+        Terminal.replaceinSB(mergeSortSolutionStringBuilder, "$INITARRAY$", Terminal.printArray(a));
 
-        // zurückkopieren
-        for (int i = 0; i <= r - l; i++) {
-            numbers[l + i] = buffer[i];
-        }
-    }
+        printSplit(a, a.length);
+        printMerge(a);
 
-    public static void sort(int[] a) {
-        sort(a, 0, a.length - 1, 0);
-    }
+        Terminal.replaceinSB(exerciseStringBuilder, "%$MergeSort$", "\\cellcolor{tumgadPurple}");
+        Terminal.replaceinSB(solutionStringBuilder, "%$MergeSort$", "\\cellcolor{tumgadRed}");
 
-    public static void main(String[] args) {
-        int[] a = new int[]{21, 82, 2, 34, 79, 9, 65, 60, 95, 38, 62, 88, 90, 7, 12, 124};
-        System.out.println(printSplit(a, a.length));
-        System.out.println(printMerge(a));
+        Terminal.replaceinSB(exerciseStringBuilder, "%$MERGESORT$", "\\newpage\n" + mergeSortExerciseStringBuilder.toString());
+        Terminal.replaceinSB(solutionStringBuilder, "%$MERGESORT$", "\\newpage\n" + mergeSortSolutionStringBuilder.toString());
+
+        Terminal.saveToFile("docs/Exercises.tex", exerciseStringBuilder);
+        Terminal.saveToFile("docs/Solutions.tex", solutionStringBuilder);
     }
 
     static String printSplit(int[] a, int m) {
         if (m == 0) {
             return "";
+        }
+        if (m == a.length) {
+            return printSplit(a, m / 2);
         }
         String ret = "" + a[0];
         for (int i = 1; i < a.length; i++) {
@@ -78,25 +49,65 @@ public class MergeSort {
                 ret += ", " + a[i];
             }
         }
+        Terminal.replaceinSB(mergeSortSolutionStringBuilder, "%$SORTINGARRAY$", "\n" +
+                "\\underline{Split:}\n" +
+                "    \\begin{center}\n" +
+                "       \\color{tumgadRed}" +
+                "        \\begin{tabular}{|c|}\n" +
+                "            \\hline\n" +
+                ret.replace("|", "$|$") + "\\\\\n" +
+                "            \\hline\n" +
+                "        \\end{tabular}\n" +
+                "        \\vspace{10px}\n" +
+                "    \\end{center}\n" +
+                "%$SORTINGARRAY$");
         return ret + "\n" + printSplit(a, m / 2);
+    }
+
+    public static int[] generateRandomMSArray() {
+        int arraySize = 16;
+        ArrayList<Integer> list = new ArrayList<>(arraySize);
+        for (int i = 0; i <= 49; i++) {
+            list.add(i);
+        }
+        int[] a = new int[arraySize];
+        for (int count = 0; count < arraySize; count++) {
+            a[count] = list.remove((int) (Math.random() * list.size()));
+        }
+        return a;
     }
 
     static String printMerge(int[] a) {
         String result = "";
         for (int i = 0; i <= a.length / Math.pow(2, i) + 2; i++) {
-            result += printMerge(a, i + 1) + "\n";
+            // TODO 09/03/2020 sebas: removed concatenation to make latex replacement work better (temporary)
+            result = printMerge(a, i + 1) + "\n";
+            Terminal.replaceinSB(mergeSortSolutionStringBuilder, "%$SORTINGARRAY$", "\n" +
+                    "\\underline{Merge:}\n" +
+                    "    \\begin{center}\n" +
+                    "       \\color{tumgadRed}" +
+                    "        \\begin{tabular}{|c|}\n" +
+                    "            \\hline\n" +
+                    result.replace("|", "$|$") + "\\\\\n" +
+                    "            \\hline\n" +
+                    "        \\end{tabular}\n" +
+                    "        \\vspace{10px}\n" +
+                    "    \\end{center}\n" +
+                    "%$SORTINGARRAY$");
         }
         return result;
     }
 
     static String printMerge(int[] a, int depth) {
         //depth starts at 1 for pairs. 2 for groups of 4's
-        int level = (int)Math.pow(2, depth);
+        int level = (int) Math.pow(2, depth);
         for (int i = 0, r = level; i < r; i = r, r += level) {
-            if (i >= a.length)
+            if (i >= a.length) {
                 break;
-            if (r > a.length)
+            }
+            if (r > a.length) {
                 r = a.length;
+            }
 
             for (int k = i; k < r; k++) {
                 for (int j = i; j < r - 1; j++) {
@@ -113,12 +124,12 @@ public class MergeSort {
 
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < a.length - 1; i++) {
-            if(i % level == level-1) {
+            if (i % level == level - 1) {
                 result.append(a[i] + " ");
                 result.append("| ");
                 continue;
             }
-           result.append(a[i] + ", ");
+            result.append(a[i] + ", ");
         }
         result.append(a[a.length - 1]);
         return result.toString();
