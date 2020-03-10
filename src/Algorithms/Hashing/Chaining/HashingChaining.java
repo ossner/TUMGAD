ad7ff8d20d983490e238641a033d3570d99fd4dd
Sebastian Ossner <github@sebastianoner.com>
2020-03-10 11:24:32 +0100

    add generation infrastructure to hashing chaining

--- src/Algorithms/Hashing/Chaining/HashingChaining.java
+++ src/Algorithms/Hashing/Chaining/HashingChaining.java
@@ -1,5 +1,32 @@
 package Algorithms.Hashing.Chaining;
 
-public class HashingChaining {
+import Algorithms.Hashing.Double.DoubleHashing;
+import DataStructures.Terminal;
 
+public class HashingChaining {
+    static StringBuilder hashingChainingExerciseStringBuilder;
+    static StringBuilder hashingChainingSolutionStringBuilder;
+    static int[] hashFunction = DoubleHashing.generateH1Function();
+    public static void main(String[] args) {
+
+    }
+
+    public static void generateExercise() {
+        hashingChainingExerciseStringBuilder = Terminal.readFile("src/Algorithms/Hashing/Double/HashingChainingExerciseTemplate.tex");
+        hashingChainingSolutionStringBuilder = Terminal.readFile("src/Algorithms/Hashing/Double/HashingChainingSolutionTemplate.tex");
+
+        hashFunction = DoubleHashing.generateH1Function();
+
+        StringBuilder exerciseStringBuilder = Terminal.readFile("docs/Exercises.tex");
+        StringBuilder solutionStringBuilder = Terminal.readFile("docs/Solutions.tex");
+
+        Terminal.replaceinSB(exerciseStringBuilder, "%$HashingChaining$", "\\cellcolor{tumgadPurple}");
+        Terminal.replaceinSB(solutionStringBuilder, "%$HashingChaining$", "\\cellcolor{tumgadRed}");
+
+        Terminal.replaceinSB(exerciseStringBuilder, "%$HASHINGCHAINING$", "\\newpage\n" + hashingChainingExerciseStringBuilder.toString());
+        Terminal.replaceinSB(solutionStringBuilder, "%$HASHINGCHAINING$", "\\newpage\n" + hashingChainingSolutionStringBuilder.toString());
+
+        Terminal.saveToFile("docs/Exercises.tex", exerciseStringBuilder);
+        Terminal.saveToFile("docs/Solutions.tex", solutionStringBuilder);
+    }
 }
